7d0f168eac318a333e7e37556d47ef2870f8bdf7
shrutikhadake <148204864+shrutikhadake@users.noreply.github.com>
2023-12-10 11:00:26 +0530

    Update using_inheritance.cpp

--- using_inheritance.cpp
+++ using_inheritance.cpp
@@ -3,37 +3,37 @@
 using namespace std;
 class A
 {
-   public:
+    public:
     int num,a;
-public:
-void show()
-{
-    cout<<"Enter the number of elelments"<<endl;
-    cin>>num;
-    cout<<"enter the numbers"<<endl;
-     cin>>a;  
-    for(int i=0;i<=num;i++)
-     {
-       //cout<<"enter the numbers"<<endl;
-       //cin>>a;  
-     }
-}
+    double sum=0,avg=0;
+    public:
+    void get_sum()
+    {
+        cout<<"Enter the number of elements:"<<endl;
+        cin>>num;
+        cout<<"Enter the elements:"<<endl;
+        for(int i=0;i<num;i++)
+        {
+            cin>>a;
+            sum=sum+a;
+            
+        }
+        avg=sum/num;
+    }
 };
 class B:public A
 {
-   public:
-    double sum=0,avg=0;
-public: 
-    void sum_of()
-   {
-    sum=sum+a;
-    cout<<"Average is:"<<sum<<endl;
-   }
+    public:
+   void  show_avg()
+     {
+        cout<<"The sum is :"<<sum<<endl;
+        cout<<"The average is:"<<avg<<endl;
+     }
 };
 int main()
 {
-   B obj;
-    obj.show();
-    obj.sum_of();
-    
+    A a1;
+    B b1;
+    b1.get_sum();
+    b1.show_avg();
 }
